96a491652e6e0f9a4fe284ec9f6ead7313a8f838
Joel Trottier-Hebert <iamquebecois@gmail.com>
2014-11-15 09:04:08 -0500

    Added a basic websocket communications structure for the game

--- PokerPlanningClient/web/pokerplanning.dart
+++ PokerPlanningClient/web/pokerplanning.dart
@@ -1,11 +1,15 @@
 import 'dart:html';
+import 'dart:async';
+import 'dart:convert';
 
 Map<String, String> players = {
 };
 String myName;
+WebSocket ws;
 
 void main() {
   querySelector("#loginButton").onClick.listen(login);
+  initWebSocket();
 }
 
 void hideLoginForm() {
@@ -25,9 +29,40 @@ void login(MouseEvent e) {
   InputElement nameInput = querySelector("#nameInput");
   myName = nameInput.value;
 
-  if(myName.isEmpty) return;
+  if (myName.isEmpty) return;
+
+  var loginInfo = {'login' : myName};
+
+  ws.send(JSON.encode(loginInfo));
 
   hideLoginForm();
   showLoginSuccessful();
   showGame();
+}
+
+outputMsg(String msg) {
+  print(msg);
+}
+
+void initWebSocket([int retrySeconds = 2]) {
+  var reconnectScheduled = false;
+
+  outputMsg("Connecting to websocket");
+  ws = new WebSocket('ws://127.0.0.1:4040/ws');
+
+  ws.onOpen.listen((e) {
+    outputMsg('Connected');
+  });
+
+  ws.onClose.listen((e) {
+    outputMsg('Websocket closed, retrying in $retrySeconds seconds');
+  });
+
+  ws.onError.listen((e) {
+    outputMsg("Error connecting to ws");
+  });
+
+  ws.onMessage.listen((MessageEvent e) {
+    outputMsg('Received message: ${e.data}');
+  });
 }
--- PokerPlanningServer/main.dart
+++ PokerPlanningServer/main.dart
@@ -1,3 +1,38 @@
-void main() {
-  print("hello");
+import 'dart:io';
+import 'dart:convert';
+
+WebSocket ws;
+Map<String, String> game = {
+};
+
+printGame() {
+  print("The players connected are : ");
+  game.forEach((k, v) => print("$k and their current card choice is: $v"));
 }
+
+void handleMessage(socket, message) {
+  print("Received : " + message);
+
+  Map json = JSON.decode(message);
+
+  var login = json["login"];
+
+  if (login != null) {
+    game.putIfAbsent(login, () => "");
+    socket.add("$login successfully connected");
+  }
+
+  printGame();
+}
+
+void main() {
+  HttpServer.bind('127.0.0.1', 4040).then((server) {
+    server.listen((HttpRequest req) {
+      if (req.uri.path == '/ws') {
+        WebSocketTransformer.upgrade(req)
+          ..then((socket) => socket.listen((msg) => handleMessage(socket, msg)));
+      }
+    }).onError((e) => print("An error occurred."));
+  });
+}
+
